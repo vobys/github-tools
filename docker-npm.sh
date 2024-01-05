8684bf7f279f30b6215fd8cb1dfb5f2d151802e4
Alfredo Fialho <alfoltran@gmail.com>
2024-01-05 10:33:26 -0300

    Adding script with docker support.

---
+++ docker-npm.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#
+# Use:
+# ./docker-npm <<COMMANDS>>
+#
+
+if [[ "$#" -lt 1 ]] ; then
+    echo "Use: ./docker-npm <<COMMANDS>>"
+    exit 1
+fi
+
+docker run -it --rm --name github-nodejs -v "$PWD":/usr/src/app -w /usr/src/app node:9 npm "$@"
