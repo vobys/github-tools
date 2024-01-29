ed2bad277acf0181641a84f1b1171d54c2bd3085
Alfredo Fialho <alfoltran@gmail.com>
2024-01-29 13:48:51 -0300

    Adjusts in script with docker support.

--- docker-npm.sh
+++ docker-npm.sh
@@ -10,4 +10,4 @@ if [[ "$#" -lt 1 ]] ; then
     exit 1
 fi
 
-docker run -it --rm --name github-nodejs -v "$(pwd)":/usr/src/app -w /usr/src/app node:9 npm "$@"
+docker run -i --rm --name github-nodejs -v "$(pwd)":/usr/src/app -w /usr/src/app node:9 npm "$@"
